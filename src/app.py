f806ec93194258dbcf9617bfe838f9331823286f
Leticia <leticiagabriellourenco@gmail.com>
2020-05-13 16:29:29 -0700

    Write GET and POST endpoints for artist

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from src.database.models import setup_db
+from flask import Flask, jsonify, request, abort
+from src.database.models import setup_db, Actor, Movie
 from flask_cors import CORS
 import psycopg2
 
@@ -17,6 +17,69 @@ def index():
     return jsonify({
         'message': 'success'
     })
+
+@app.route('/actors')
+def get_actors():
+    # query all actors from table
+    raw_actors = Actor.query.all()
+    # extract information from actors using its format() method
+    actors = [actor.format() for actor in raw_actors]
+    # checks for presence of results in actors list
+    if not len(actors):
+        abort(404)
+
+    return jsonify({
+        'success': True,
+        'actors': actors
+    })
+
+@app.route('/actors', methods=['POST'])
+def create_actor():
+    data = request.get_json()
+    # checks for presence of results in actors list
+    if not data:
+        abort(400)
+
+    # retrieves data from request
+    name = data['name']
+    age = data['age']
+    gender = data['gender']
+    # creates a new instance of actor passing the retrieved data
+    actor = Actor(name=name, age=age, gender=gender)
+    try:
+        actor.insert()
+    except:
+        abort(422)
+
+    return jsonify({
+        'success': True,
+        'actor': actor.format()
+    })
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+    }), 400
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "not found"
+    }), 404
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
 
 
 if __name__ == '__main__':
