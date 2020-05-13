943756467ea046d157a7b8169f9214436f53478a
Leticia <leticiagabriellourenco@gmail.com>
2020-05-13 15:32:42 -0700

    Setup db migrations

--- src/app.py
+++ src/app.py
@@ -1,7 +1,15 @@
 from flask import Flask, jsonify
 from src.database.models import setup_db
+from flask_cors import CORS
+import psycopg2
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    app_ = Flask(__name__)
+    CORS(app_)
+    return app_
+
+
+app = create_app()
 setup_db(app)
 
 @app.route('/')
@@ -9,3 +17,7 @@ def index():
     return jsonify({
         'message': 'success'
     })
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- src/database/models.py
+++ src/database/models.py
@@ -1,10 +1,76 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer, DateTime
+from flask_migrate import Migrate
 
-db_path = 'postgres://leticia:5678@localhost:5432/MOCK'
+db_path = 'postgres://leticia:5678@localhost:5432/casting_agency'
 db = SQLAlchemy()
+migrate = Migrate()
 
 def setup_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = db_path
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
+    db.create_all()
+    migrate.init_app(app, db)
+
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(DateTime)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
