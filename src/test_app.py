0649464f01383fcf1d82d024f80c9b2b2a157c57
Leticia <leticiagabriellourenco@gmail.com>
2020-05-13 18:10:11 -0700

    Write unit test for get and create actors

--- src/test_app.py
+++ src/test_app.py
@@ -18,6 +18,32 @@ class CastingAgencyTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data['success'], True)
 
+    def test_404_get_actors(self):
+        response = self.client.get('/aktor')
+        data = json.loads(response.data)
+        # check response status code
+        self.assertEqual(response.status_code, 404)
+
+    def test_create_actor(self):
+        test_actor = {
+            "name": "Sandra Bullock",
+            "age": 45,
+            "gender": "Non-binary"
+        }
+        response = self.client.post('/actors', json=test_actor)
+        data = json.loads(response.data)
+        # assert that data actors list is not empty
+        self.assertTrue(len(data['actor']))
+        # check response message and its code
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_400_create_actor(self):
+        test_actor = {}
+        response = self.client.post('/actors', json=test_actor)
+        data = json.loads(response.data)
+        # check response status code
+        self.assertEqual(response.status_code, 400)
 
 if __name__ == "__main__":
     unittest.main()
